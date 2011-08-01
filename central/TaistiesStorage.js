ba686b6d81aedc33c3d5b9d2004386f468b185a3
Anton Belousov <anton.n.belousov@gmail.com>
2011-08-01 22:21:00 +0400

    RF: extracted getting all taisties to separate method

--- central/TaistiesStorage.js
+++ central/TaistiesStorage.js
@@ -2,27 +2,36 @@ var TaistiesStorage = (function() {
 	function getTaistiesForUrl(url, callBack) {
 		var taisties = []
 
-		//TODO: move to separate method getAllTaisties
-		var routes = {
-			'lenta\\.ru': {
-				css: '.hidden { display: none !important }',
-				jslib: [],
-				js: "var settings = {\n\tpreferred: ['/science/', '/internet/', '/digital/', '/game/']\n}\n\nvar nav = document.querySelector('td.nav')\nvar groups = [].slice.call(nav.querySelectorAll('div.group'), 0)\n\ngroups.forEach(function(group) {\n\tvar links = [].slice.call(group.querySelectorAll('a'), 0)\n\tvar hiddenCount = 0\n\tlinks.forEach(function(link) {\n\t\tvar href = link.getAttribute('href')\n\t\tif (settings.preferred.indexOf(href) == -1) {\n\t\t\tlink.parentNode.className += ' hidden'\n\t\t\thiddenCount++\n\t\t}\n\t})\n\n\tif (hiddenCount == links.length) {\n\t\tgroup.className += ' hidden'\n\t}\n})"
-			}
-		}
-		localStorage.setItem('routes', JSON.stringify(routes))
+		var allTaistiesByUrlRegexps = this.getAllTaisties();
 
-		for (var route in routes) {
+		for (var route in allTaistiesByUrlRegexps) {
 			var routeRE = new RegExp(route, 'g')
 			if (routeRE.test(url)) {
-				taisties.push(routes[route])
+				taisties.push(allTaistiesByUrlRegexps[route])
 			}
 		}
 
 		callBack(taisties)
 	}
 
+	function getAllTaisties() {
+		var allTaistiesByUrlRegExps = {
+			'lenta\\.ru': {
+				css: '.hidden { display: none !important }',
+				jslib: [],
+				js: "var settings = {\n\tpreferred: ['/science/', '/internet/', '/digital/', '/game/']\n}\n\nvar nav = document.querySelector('td.nav')\nvar groups = [].slice.call(nav.querySelectorAll('div.group'), 0)\n\ngroups.forEach(function(group) {\n\tvar links = [].slice.call(group.querySelectorAll('a'), 0)\n\tvar hiddenCount = 0\n\tlinks.forEach(function(link) {\n\t\tvar href = link.getAttribute('href')\n\t\tif (settings.preferred.indexOf(href) == -1) {\n\t\t\tlink.parentNode.className += ' hidden'\n\t\t\thiddenCount++\n\t\t}\n\t})\n\n\tif (hiddenCount == links.length) {\n\t\tgroup.className += ' hidden'\n\t}\n})"
+			}
+		}
+
+		//сохраним в локальном хранилище - пока просто для демонстрации его работы (используется в options.html)
+		localStorage.setItem('routes', JSON.stringify(allTaistiesByUrlRegExps));
+
+		return allTaistiesByUrlRegExps;
+
+	}
+
 	return {
-		getTaistiesForUrl: getTaistiesForUrl
+		getTaistiesForUrl: getTaistiesForUrl,
+		getAllTaisties: getAllTaisties
 	}
 })()
