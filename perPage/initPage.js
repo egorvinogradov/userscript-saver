3cec32f450970109cd7d62d43a4acddcf94c6780
Anton Belousov <anton.n.belousov@gmail.com>
2011-08-02 21:13:50 +0400

    RF: incapsulated initialization logic into anonymous closure

--- perPage/initPage.js
+++ perPage/initPage.js
@@ -1,12 +1,15 @@
-taistieEmbedder = new TaistieEmbedder();
+(function() {
+	taistieEmbedder = new TaistieEmbedder();
 
-function req_dispatcher(request, sender, callback) {
-	if (request.action == 'bundleReady') {
-		taistieEmbedder.embedTaistie(document, request.type, request.body)
+	function req_dispatcher(request, sender, callback) {
+		if (request.action == 'bundleReady') {
+			taistieEmbedder.embedTaistie(document, request.type, request.body)
+		}
+
+		callback({})
 	}
 
-	callback({})
-}
-chrome.extension.onRequest.addListener(req_dispatcher);
+	chrome.extension.onRequest.addListener(req_dispatcher);
 
-chrome.extension.sendRequest({action: 'getTaisties', url: document.location.href})
+	chrome.extension.sendRequest({action: 'getTaisties', url: document.location.href})
+}());
