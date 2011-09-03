495015ed7e224796fd82b43af5dfe68f71c6f6e4
Anton Belousov <anton.n.belousov@gmail.com>
2011-09-03 14:20:57 +0400

    first version of habr scroller

--- central/CentralDevelopedTaistie.js
+++ central/CentralDevelopedTaistie.js
@@ -2,12 +2,34 @@
 //to use this file write needed code in corresponding local variables (write js code in js_func function body)
 
 DevelopedTaistie = function() {
-	var siteRegexp = 'your_tuned_site\\.com',
+	var siteRegexp = 'habrahabr\\.ru',
 		css = '',
-		jslib = [],
+		jslib = ['lib/jquery.js'],
 		jsFunction = function() {
 			//place js code here
 
+			function initScroller(contentBlock, linkBlock) {
+				$(linkBlock).click(function() {
+					$(contentBlock).load($(this).attr('href') + ' ' + contentBlock + ' > *', function() {
+						initAllScrollers()})
+					return false
+				})
+			}
+
+			function initAllScrollers() {
+                var scrolledBlockDescriptions = [
+                    {
+                        contentBlock: '#main-content',
+                        linkBlock: '.page-nav a'
+                    }
+                ]
+
+				$.each(scrolledBlockDescriptions, function(index, scrolledBlockDescription) {
+					initScroller(scrolledBlockDescription.contentBlock, scrolledBlockDescription.linkBlock)
+				})
+			}
+
+            initAllScrollers();
 		}
 
 	var js = '(' + jsFunction.toString() + ')()';
