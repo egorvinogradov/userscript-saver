3be860ebbffac5dd10291f819ca227a18d12727e
Anton Belousov <anton.n.belousov@gmail.com>
2011-09-23 19:38:56 +0400

    added Inheritance.extend to enable inheritance

---
+++ core/Inheritance.js
@@ -0,0 +1,9 @@
+Inheritance = {
+	extend: function(Child, Parent) {
+		var childPrototypeBasis = function() { }
+		childPrototypeBasis.prototype = Parent.prototype
+		Child.prototype = new childPrototypeBasis()
+		Child.prototype.constructor = Child
+		Child.__base = Parent.prototype
+	}
+}
