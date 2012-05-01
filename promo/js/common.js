420d1b7048ba55fc18469daae56b4703122eda8e
Egor Vinogradov <egor@ostrovok.ru>
2012-05-01 20:25:53 +0400

    promo page js fix

--- promo/js/common.js
+++ promo/js/common.js
@@ -7,7 +7,7 @@ $(function(){
             slideCount: 3
         },
         els: {
-            body: $('.page'),
+            body:       $('.page'),
             team: {
                 block:  $('.team'),
                 popup:  $('.team__popup'),
@@ -16,12 +16,13 @@ $(function(){
             },
             slides: {
                 headers:    $('.capabilities__header'),
-                slides:     $('.capabilities__slide-steps'),
+                wrapper:    $('.capabilities__slides-wrapper'),
                 switchers:  $('.capabilities__switcher'),
+
                 headerContainer:    $('.capabilities__headers-list'),
                 slideContainer:     $('.capabilities__slides'),
                 switcherContainer:  $('.capabilities__switcher'),
-                fade: {
+                fading: {
                     left:   $('.capabilities__headers-fading-left'),
                     right:  $('.capabilities__headers-fading-right')
                 }
@@ -32,11 +33,11 @@ $(function(){
                 active:     'm-active',
                 left:       'm-left',
                 right:      'm-right',
-                invisible:  'm-invisible'
+                invisible:  'm-invisible',
+                hover:      'm-hover'
             },
-            slideActive:    'm-active',
-            switcherActive: 'm-active',
-            fadeInvisible:  'm-invisible'
+            switcherActive:     'm-active',
+            fadingInvisible:    'm-invisible'
         },
 
         showTeam: function(){
@@ -55,7 +56,7 @@ $(function(){
             return current - 1 > -1 ? current - 1 : this.settings.slideCount - 1;
         },
         getCurrentSlide: function(){
-            return this.els.slides.slides.filter('.' + this.classes.slideActive).index();
+            return this.els.slides.headers.filter('.' + this.classes.headers.active).index();
         },
         switchSlide: function(number){
 
@@ -63,80 +64,118 @@ $(function(){
                     pageWidth: 1000,
                     headerSpace: 40
                 },
-                els = {
-                    header: this.els.slides.headers.eq(number),
-                    slide: this.els.slides.slides.eq(number),
-                    switcher: this.els.slides.slides.eq(number),
-                    fade: this.els.slides.fade.left.add(this.els.slides.fade.left)
-                },
+                header = this.els.slides.headers.eq(number),
+                fading = this.els.slides.fading.all,
                 animate = function(){
 
-                    var headerWidth = els.header.width(),
-                        headerIndents = settings.pageWidth - ( settings.pageWidth - headerWidth ) / 2 + settings.headerSpace;
+                    settings.headerIndents = settings.pageWidth - ( settings.pageWidth - header.width() ) / 2 + settings.headerSpace;
 
-                    settings.headerIndents = headerIndents;
-                    els.header
-                        .next().css({ left: headerIndents }).end()
-                        .prev().css({ right: headerIndents });
+                    header
+                        .next().css({ left: settings.headerIndents }).end()
+                        .prev().css({ right: settings.headerIndents }).end()
+                        .siblings().hide().fadeIn(600);
 
-                    els.slide.css({ left: '100%' }).animate({ left: 0 });
-                    els.header.siblings()
-                        .hide().fadeIn().css({ display: '' });
+                    fading.width( ( settings.pageWidth - header.width() ) / 2 );
                 };
 
-            this.els.slides.slides.removeClass(this.classes.slideActive);
             this.els.slides.headers
                 .removeClass(this.classes.headers.active)
                 .removeClass(this.classes.headers.left)
+                .removeClass(this.classes.headers.hover)
                 .removeClass(this.classes.headers.right)
-                .removeAttr('style')
-                .addClass(this.classes.headers.invisible);
+                .addClass(this.classes.headers.invisible)
+                .removeAttr('style');
 
-            els.slide.addClass(this.classes.slideActive);
-            els.header.addClass(this.classes.headers.active).removeClass(this.classes.headers.invisible)
-                .next().addClass(this.classes.headers.right).removeClass(this.classes.headers.invisible).end()
-                .prev().addClass(this.classes.headers.left).removeClass(this.classes.headers.invisible);
+            this.els.slides.wrapper
+                .animate({ left: number * -1000 });
 
             this.els.slides.switchers
                 .removeClass(this.classes.switcherActive)
                 .eq(number).addClass(this.classes.switcherActive);
+
+            header
+                .addClass(this.classes.headers.active).removeClass(this.classes.headers.invisible)
+                .next().addClass(this.classes.headers.right).removeClass(this.classes.headers.invisible).end()
+                .prev().addClass(this.classes.headers.left).removeClass(this.classes.headers.invisible);
+
+            fading
+                .removeClass(this.classes.fadingInvisible);
+
+            console.log('lol ', number);
+
+            number === 0 && this.els.slides.fading.left.addClass(this.classes.fadingInvisible);
+            number === 2 && this.els.slides.fading.right.addClass(this.classes.fadingInvisible);
 
             setTimeout(animate, 0);
 
         },
         init: function(){
 
+            var context = this,
+                slideFirst = function(){
+                    context.switchSlide( context.getNextSlide( context.getCurrentSlide() ) );
+                },
+                handlers = {
+
+                    fadingClick: function(event){
+
+                        var className = $(event.target).attr('class'),
+                            current =   this.getCurrentSlide(),
+                            isLeft =    className.indexOf('left') > -1,
+                            isRight =   className.indexOf('right') > -1,
+                            slide = isLeft
+                                ? this.getPrevSlide(current)
+                                : isRight
+                                    ? this.getNextSlide(current)
+                                    : false;
+
+                        this.switchSlide(slide);
+                    },
+                    fadingHover: function(event){
+
+                        var className = $(event.target).attr('class'),
+                            hover =     this.classes.headers.hover,
+                            isLeft =    className.indexOf('left') > -1,
+                            isRight =   className.indexOf('right') > -1,
+                            selector =
+                                isLeft  && this.classes.headers.left ||
+                                isRight && this.classes.headers.right,
+
+                            header = this.els.slides.headers.filter('.' + selector);
+
+                        event.type === 'mouseover'  && header.addClass(hover);
+                        event.type === 'mouseout'   && header.removeClass(hover);
+                    }
+                };
+
+
+            this.els.slides.fading.all = this.els.slides.fading.left.add(this.els.slides.fading.right);
             this.els.team.link.click($.proxy(this.showTeam, this));
             this.els.body.bind('click keydown', $.proxy(function(event){
 
-                var condition = ( event.type === 'keydown' && event.which === 27 ) ||
-                                ( event.type === 'click' && $(event.target).is(this.els.team.close) );
+                var target = $(event.target),
+                    condition = ( event.type === 'keydown' && event.which === 27 ) ||
+                                ( event.type === 'click' && ( target.is(this.els.team.close) || target.is(this.els.team.block) ) );
 
                 condition && this.hideTeam();
                 event.stopPropagation();
 
             }, this));
 
+
             this.els.slides.switchers.click($.proxy(function(event){
                 this.switchSlide( $(event.target).index() );
             }, this));
 
-            this.els.slides.fade.left
-                .add(this.els.slides.headers.filter('.' + this.classes.headers.left))
-                .click($.proxy(function(){
-                    this.switchSlide( this.getPrevSlide(this.getCurrentSlide()) );
-                }, this));
 
-            this.els.slides.fade.right
-                .add(this.els.slides.headers.filter('.' + this.classes.headers.right))
-                .click($.proxy(function(){
-                    this.switchSlide( this.getNextSlide(this.getCurrentSlide()) );
-                }, this));
+            this.els.slides.fading.all
+                .click($.proxy(handlers.fadingClick, this))
+                .bind('mouseover mouseout', $.proxy(handlers.fadingHover, this));
+
+            setTimeout(slideFirst, 1600);
         }
     };
 
     Promo.init();
-
-    window.promo = Promo; // TODO: remove;
 
 });
